2f10085433efe430d18615c35adc0df8b7abb3e4
saburlaev <mr.saburlaev@mail.ru>
2023-09-18 14:05:54 +0300

    Второе задание выполнено

---
+++ ServerController/ServerController/Controllers/CalculatorController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ServerController.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        [HttpPost]
+        public string Calc(int arg1, int arg2)
+        {
+            decimal result = arg1 + arg2;
+            return $"{result}";
+        }
+    }
+}
