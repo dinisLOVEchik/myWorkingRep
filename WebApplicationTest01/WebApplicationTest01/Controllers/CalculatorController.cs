35e8abd859468d16abb31cd009f53f42c7f61b43
saburlaev_denis <mr.saburlaev@mail.ru>
2023-09-19 21:40:19 +0300

    Реализация с применением сервиса

--- WebApplicationTest01/WebApplicationTest01/Controllers/CalculatorController.cs
+++ WebApplicationTest01/WebApplicationTest01/Controllers/CalculatorController.cs
@@ -9,20 +9,11 @@ namespace WebApplicationTest01.Controllers
     [ApiController]
     public class CalculatorController : ControllerBase
     {
+        CalculatorService calculatorService = new CalculatorService();
         [HttpPost]
         public string Calc(CalcRequest calcRequest)
         {
-            decimal res = 0;
-            if (calcRequest.op.Equals(Operation.add.ToString()))
-                res = calcRequest.arg1 + calcRequest.arg2;
-            else if (calcRequest.op.Equals(Operation.substruct.ToString()))
-                res = calcRequest.arg1 - calcRequest.arg2;
-            else if (calcRequest.op.Equals(Operation.multiply.ToString()))
-                res = calcRequest.arg1 * calcRequest.arg2;
-            else if (calcRequest.op.Equals(Operation.devide.ToString()))
-                res = calcRequest.arg1 / calcRequest.arg2;
-
-            return $"{res}";
+            return calculatorService.Calc(calcRequest.arg1, calcRequest.arg2, calcRequest.op);
         }
     }
 }
