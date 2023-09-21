b21715891f1cb225ac797f5b676568ce1f18c239
saburlaev <mr.saburlaev@mail.ru>
2023-09-21 09:21:29 +0300

    Пересобрал проект

--- PersonalFinance.Api/PersonalFinance.Tests/CalcTests.cs
+++ PersonalFinance.Api/PersonalFinance.Tests/CalcTests.cs
@@ -1,3 +1,4 @@
+using PersonalFinance.Services;
 namespace PersonalFinance.Tests
 {
     public class CalcTests
@@ -8,9 +9,63 @@ namespace PersonalFinance.Tests
         }
 
         [Test]
-        public void Test1()
+        public void AddCalcTest()
         {
-            Assert.Pass();
+            int x = 10;
+            int y = 2;
+            string op = "add";
+            string expected = "12";
+
+
+            CalculatorService calculatorService = new CalculatorService();
+            string actual = calculatorService.Calc(x, y, op);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void SubstructCalcTest()
+        {
+            int x = 10;
+            int y = 2;
+            string op = "substruct";
+            string expected = "8";
+
+
+            CalculatorService calculatorService = new CalculatorService();
+            string actual = calculatorService.Calc(x, y, op);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void MultiplyCalcTest()
+        {
+            int x = 10;
+            int y = 2;
+            string op = "multiply";
+            string expected = "20";
+
+
+            CalculatorService calculatorService = new CalculatorService();
+            string actual = calculatorService.Calc(x, y, op);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void DevideCalcTest()
+        {
+            int x = 10;
+            int y = 2;
+            string op = "devide";
+            string expected = "5";
+
+
+            CalculatorService calculatorService = new CalculatorService();
+            string actual = calculatorService.Calc(x, y, op);
+
+            Assert.AreEqual(expected, actual);
         }
     }
 }
