5fe279b951a84fb4c71071b5c1d252af9306b32a
saburlaev <mr.saburlaev@mail.ru>
2023-09-21 08:26:08 +0300

    Пересобрал проект Тест

--- PersonalFinance.Api/PersonalFinance.Tests/CalcTests.cs
+++ PersonalFinance.Api/PersonalFinance.Tests/CalcTests.cs
@@ -1,6 +1,3 @@
-using Microsoft.VisualStudio.TestPlatform;
-using PersonalFinance.Services;
-
 namespace PersonalFinance.Tests
 {
     public class CalcTests
@@ -11,59 +8,9 @@ namespace PersonalFinance.Tests
         }
 
         [Test]
-        public void AddCalcTest()
+        public void Test1()
         {
-            int x = 10;
-            int y = 2;
-            string op = "add";
-            string expected = "12";
-
-            CalculatorService calculator = new CalculatorService();
-            string actual = calculator.Calc(x, y, op);
-
-            Assert.AreEqual(expected, actual);
-        }
-
-        [Test]
-        public void SubstructCalcTest()
-        {
-            int x = 10;
-            int y = 8;
-            string op = "substruct";
-            string expected = "2";
-
-            CalculatorService calculator = new CalculatorService();
-            string actual = calculator.Calc(x, y, op);
-
-            Assert.AreEqual(expected, actual);
-        }
-
-        [Test]
-        public void MultiplyCalcTest()
-        {
-            int x = 10;
-            int y = 2;
-            string op = "multiply";
-            string expected = "20";
-
-            CalculatorService calculator = new CalculatorService();
-            string actual = calculator.Calc(x, y, op);
-
-            Assert.AreEqual(expected, actual);
-        }
-
-        [Test]
-        public void devideCalcTest()
-        {
-            int x = 10;
-            int y = 2;
-            string op = "devide";
-            string expected = "5";
-
-            CalculatorService calculator = new CalculatorService();
-            string actual = calculator.Calc(x, y, op);
-
-            Assert.AreEqual(expected, actual);
+            Assert.Pass();
         }
     }
 }
