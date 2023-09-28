608ee997097f4f0e03dc238b0816989d1be70f4d
saburlaev <mr.saburlaev@mail.ru>
2023-09-28 16:26:31 +0300

    Работа над созданием csv-файла

---
+++ PersonalFinance.Api/PersonalFinance.Tests/MockExchangeRatesGenerationService.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalFinance.Tests
+{
+    internal class MockExchangeRatesGenerationService
+    {
+        const int MIN_VALUE = 1;
+        const int MAX_VALUE = 1000;
+        public List<ExcangeRate> MockExchangeRatesGenerator(List<string> isoCodesOfRates)
+        {
+            List<ExcangeRate> result = new List<ExcangeRate>();
+
+            for (int currency1 = 0; currency1 < isoCodesOfRates.Count; currency1++)
+            {
+                string x = isoCodesOfRates[currency1];
+                for (int currency2 = 0; currency2 < isoCodesOfRates.Count; currency2++)
+                {
+                    string y = isoCodesOfRates[currency2];
+
+                    if (result.Count == 0 && !isoCodesOfRates[currency1].Equals(isoCodesOfRates[currency2]))
+                    {
+                        result.Add(new ExcangeRate(isoCodesOfRates[currency1], isoCodesOfRates[currency2], new Random().Next(MIN_VALUE, MAX_VALUE)));
+                    }
+                    else if (result.Count == 0)
+                    {
+                        result.Add(new ExcangeRate(isoCodesOfRates[currency1], isoCodesOfRates[currency2], MIN_VALUE));
+                    }
+                    else
+                    {
+                        if (isoCodesOfRates[currency1].Equals(isoCodesOfRates[currency2]))
+                        {
+                            result.Add(new ExcangeRate(isoCodesOfRates[currency2], isoCodesOfRates[currency1], MIN_VALUE));
+                        }
+                        else
+                        {
+                            int count = 0;
+                            foreach (ExcangeRate rate in result.ToList())
+                            {
+                                count++;
+                                if (isoCodesOfRates[currency1].Equals(rate.getValue2()) && isoCodesOfRates[currency2].Equals(rate.getValue1()))
+                                {
+                                    result.Add(new ExcangeRate(isoCodesOfRates[currency1], isoCodesOfRates[currency2], MIN_VALUE / Math.Round(rate.getRate(), 2)));
+                                    break;
+                                }
+                                else if (count == result.Count)
+                                {
+                                    result.Add(new ExcangeRate(isoCodesOfRates[currency1], isoCodesOfRates[currency2], new Random().Next(MIN_VALUE, MAX_VALUE)));
+                                    break;
+                                }
+                            }
+                        }
+
+                    }
+                };
+            };
+            return result;
+        }
+    }
+
+    public class ExcangeRate
+    {
+        private string currency1;
+        private string currency2;
+        private decimal rate;
+
+        public ExcangeRate(string value1, string value2, decimal rate)
+        {
+            this.currency1 = value1;
+            this.currency2 = value2;
+            this.rate = rate;
+        }
+        public string getValue1()
+        {
+            return currency1;
+        }
+        public string getValue2()
+        {
+            return currency2;
+        }
+        public decimal getRate()
+        {
+            return rate;
+        }
+        public void setValue1(string value)
+        {
+            this.currency1 = value;
+        }
+        public void setValue2(string value)
+        {
+            this.currency2 = value;
+        }
+        public void setRate(decimal value)
+        {
+            this.rate = value;
+        }
+    }
+}
---
+++ PersonalFinance.Api/PersonalFinance.Tests/MockServicesTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalFinance.Tests
+{
+    internal class MockServicesTests
+    {
+        public void RateGenerationTest()
+        {
+            List<string> isoCodesOfRates = new List<string>();
+            isoCodesOfRates.Add("USD");
+            isoCodesOfRates.Add("RUB");
+            isoCodesOfRates.Add("EUR");
+            isoCodesOfRates.Add("AZN");
+            isoCodesOfRates.Add("BRL");
+            isoCodesOfRates.Add("BYN");
+            isoCodesOfRates.Add("CAD");
+            isoCodesOfRates.Add("CHF");
+            isoCodesOfRates.Add("SAR");
+
+            MockExchangeRatesGenerationService service = new MockExchangeRatesGenerationService();
+
+            List<ExcangeRate>rates = service.MockExchangeRatesGenerator(isoCodesOfRates);
+
+            String _csvFile = @"G:/СиШарп/myWorkingRep/PersonalFinance.Api/Output.csv";
+
+            String separator = ",";
+            StringBuilder output = new StringBuilder();
+
+            String[] headings = { "First currency", "Second currency", "Exchange Rate" };
+            output.AppendLine(string.Join(separator, headings));
+
+            foreach (ExcangeRate rate in rates.ToList())
+            {
+                String[] newLine = { rate.getValue1(), rate.getValue2(), rate.getRate().ToString() };
+                output.AppendLine(string.Join(separator, newLine));
+            }
+
+            try
+            {
+                File.AppendAllText(_csvFile, output.ToString());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Data could not be written to the CSV file.");
+                return;
+            }
+        }
+    }
+}
