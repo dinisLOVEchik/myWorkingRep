4b4270f4ede4e0152d08b7bafdbbe45455da4956
Bashir Magomedov <bashir.magomedov@galytix.com>
2023-10-02 16:26:33 +0300

    Removing non-required files + binaries from the repo

--- PersonalFinance.Api/PersonalFinance.Tests/MockServicesTests.cs
+++ PersonalFinance.Api/PersonalFinance.Tests/MockServicesTests.cs
@@ -12,17 +12,23 @@ namespace PersonalFinance.Tests
         [Test]
         public void RateGenerationTest()
         {
-            List<string> isoCodesOfRates = ParseHtmlWithZenRows.ParseHtmlWithZenRowsMethod();
-            /*string pattern = @"[A-Z]{3}";
-            foreach(string isoCode in ParseHtmlWithZenRows.ParseHtmlWithZenRowsMethod().ToList())
+            var isoCodesOfRates = new List<string>
             {
-                if (Regex.IsMatch(isoCode, pattern) && isoCode.Length == 3)
-                {
-                    isoCodesOfRates.Add(isoCode);
-                }
-            }*/
+                "AED", "AFN", "ALL", "AMD", "ANG", "AOA", "ARS", "AUD", "AWG", "AZN", "BAM", "BBD", "BDT", "BGN", "BHD",
+                "BIF", "BMD", "BND", "BOB", "BOV", "BRL", "BSD", "BTN", "BWP", "BYN", "BZD", "CAD", "CDF", "CHE", "CHF",
+                "CHW", "CLF", "CLP", "COP", "COU", "CRC", "CUP", "CVE", "CZK", "DJF", "DKK", "DOP", "DZD", "EGP", "ERN",
+                "ETB", "EUR", "FJD", "FKP", "GBP", "GEL", "GHS", "GIP", "GMD", "GNF", "GTQ", "GYD", "HKD", "HNL", "HTG",
+                "HUF", "IDR", "ILS", "INR", "IQD", "IRR", "ISK", "JMD", "JOD", "JPY", "KES", "KGS", "KHR", "KMF", "KPW",
+                "KRW", "KWD", "KYD", "KZT", "LAK", "LBP", "LKR", "LRD", "LSL", "LYD", "MAD", "MDL", "MGA", "MKD", "MMK",
+                "MNT", "MOP", "MRU", "MUR", "MVR", "MWK", "MXN", "MXV", "MYR", "MZN", "NAD", "NGN", "NIO", "NOK", "NPR",
+                "NZD", "OMR", "PAB", "PEN", "PGK", "PHP", "PKR", "PLN", "PYG", "QAR", "RON", "RSD", "CNY", "RUB", "RWF",
+                "SAR", "SBD", "SCR", "SDG", "SEK", "SGD", "SHP", "SLE", "SLL", "SOS", "SRD", "SSP", "STN", "SVC", "SYP",
+                "SZL", "THB", "TJS", "TMT", "TND", "TOP", "TRY", "TTD", "TWD", "TZS", "UAH", "UGX", "USD", "USN", "UYI",
+                "UYU", "UYW", "UZS", "VED", "VES", "VND", "VUV", "WST", "XAF", "XAG", "XAU", "XBA", "XBB", "XBC", "XBD",
+                "XCD", "XDR", "XOF", "XPD", "XPF", "XPT", "XSU", "XTS", "XUA", "XXX", "YER", "ZAR", "ZMW", "ZWL"
+            };
 
-            MockExchangeRatesGenerationService service = new MockExchangeRatesGenerationService();
+            var service = new MockExchangeRatesGenerationService();
 
             List<ExcangeRate>rates = service.MockExchangeRatesGenerator(isoCodesOfRates);
 
