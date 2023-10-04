b1ddd4f6bde9735495a5df6b85bbcfe12593603f
saburlaev <mr.saburlaev@mail.ru>
2023-10-04 12:19:58 +0300

    Перенес класс "ExchangeRate" в отдельный класс-файл

---
+++ PersonalFinance.Api/PersonalFinance.Tests/ExchangeRate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalFinance.Tests
+{
+    public class ExchangeRate
+    {
+        public string Currency1 { get; }
+        public string Currency2 { get; }
+        public decimal Rate { get; }
+
+        public ExchangeRate(string value1, string value2, decimal rate)
+        {
+            Currency1 = value1;
+            Currency2 = value2;
+            Rate = rate;
+        }
+    }
+}
