c4c238b348388886d44123af33b696b5dc37d770
saburlaev <mr.saburlaev@mail.ru>
2023-10-04 12:22:25 +0300

    Произвел некоторый рефакторинг в "MockExchangeRatesGenerationService" и переименовал методы классе для лучшего понимания происходящего

--- PersonalFinance.Api/PersonalFinance.Tests/MockExchangeRatesGenerationService.cs
+++ PersonalFinance.Api/PersonalFinance.Tests/MockExchangeRatesGenerationService.cs
@@ -1,77 +1,72 @@
-﻿namespace PersonalFinance.Tests
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace PersonalFinance.Tests
 {
     internal class MockExchangeRatesGenerationService
     {
         const int MIN_VALUE = 1;
         const int MAX_VALUE = 1000;
-        public List<ExcangeRate> MockExchangeRatesGenerator(List<string> isoCodesOfRates)
+        public List<ExchangeRate> MockExchangeRatesGenerator(List<string> isoCodesOfRates)
         {
-            List<ExcangeRate> result = new List<ExcangeRate>();
+            List<ExchangeRate> result = new List<ExchangeRate>();
 
             for (int currency1 = 0; currency1 < isoCodesOfRates.Count; currency1++)
             {
                 for (int currency2 = 0; currency2 < isoCodesOfRates.Count; currency2++)
                 {
-                    CheckingPreliminaryListValuesBeforeAddingToFinalList(result, isoCodesOfRates, currency1, currency2);
+                    CheckAndAddingInputCoupleIfListIsEmpty(result, isoCodesOfRates, currency1, currency2);
                 };
             };
             return result;
         }
 
-        private void CheckingPreliminaryListValuesBeforeAddingToFinalList(List<ExcangeRate> result, List<string> isoCodesOfRates, int currency1, int currency2)
+        private void CheckAndAddingInputCoupleIfListIsEmpty(List<ExchangeRate> result, List<string> isoCodesOfRates, int currency1, int currency2)
         {
             if (result.Count == 0 && !isoCodesOfRates[currency1].Equals(isoCodesOfRates[currency2]))
             {
-                result.Add(new ExcangeRate(isoCodesOfRates[currency1], isoCodesOfRates[currency2], new Random().Next(MIN_VALUE, MAX_VALUE)));
+                result.Add(new ExchangeRate(isoCodesOfRates[currency1], isoCodesOfRates[currency2], new Random().Next(MIN_VALUE, MAX_VALUE)));
             }
             else if (result.Count == 0)
             {
-                result.Add(new ExcangeRate(isoCodesOfRates[currency1], isoCodesOfRates[currency2], MIN_VALUE));
+                result.Add(new ExchangeRate(isoCodesOfRates[currency1], isoCodesOfRates[currency2], MIN_VALUE));
             }
             else
             {
-                if (isoCodesOfRates[currency1].Equals(isoCodesOfRates[currency2]))
-                {
-                    result.Add(new ExcangeRate(isoCodesOfRates[currency2], isoCodesOfRates[currency1], MIN_VALUE));
-                }
-                else
-                {
-                    AdditionalCheckingOfValuesBeforeAddingToFinalList(result, isoCodesOfRates, currency1, currency2);
-                }
-
+                IfInputCoupleIsTwinsAndListIsEmptyAddCoupleToList(result, isoCodesOfRates, currency1, currency2);
             }
         }
-        private void AdditionalCheckingOfValuesBeforeAddingToFinalList (List<ExcangeRate> result, List<string> isoCodesOfRates, int currency1, int currency2)
+
+        private void IfInputCoupleIsTwinsAndListIsEmptyAddCoupleToList(List<ExchangeRate> result, List<string> isoCodesOfRates, int currency1, int currency2)
+        {
+            if (isoCodesOfRates[currency1].Equals(isoCodesOfRates[currency2]))
+            {
+                result.Add(new ExchangeRate(isoCodesOfRates[currency2], isoCodesOfRates[currency1], MIN_VALUE));
+            }
+            else
+            {
+                IterateOverListProvidedThatInputPairIsNotTwins(result, isoCodesOfRates, currency1, currency2);
+            }
+        }
+        private void IterateOverListProvidedThatInputPairIsNotTwins (List<ExchangeRate> result, List<string> isoCodesOfRates, int currency1, int currency2)
         {
             int count = 0;
-            foreach (ExcangeRate rate in result.ToList())
+            foreach (ExchangeRate rate in result.ToList())
             {
                 count++;
-                if (isoCodesOfRates[currency1].Equals(rate.Currency2) && isoCodesOfRates[currency2].Equals(rate.Currency1))
-                {
-                    result.Add(new ExcangeRate(isoCodesOfRates[currency1], isoCodesOfRates[currency2], Math.Round(MIN_VALUE / rate.Rate, 7)));
-                    break;
-                }
-                else if (count == result.Count)
-                {
-                    result.Add(new ExcangeRate(isoCodesOfRates[currency1], isoCodesOfRates[currency2], new Random().Next(MIN_VALUE, MAX_VALUE)));
-                    break;
-                }
+                AddIfThereIsMirrorCoupleOfInputPairAndAlsoAddInputPairIfItIsntInLlist(result, isoCodesOfRates, currency1, currency2, rate, count);
             }
         }
-    }
 
-    public class ExcangeRate
-    {
-        public string Currency1 { get; }
-        public string Currency2 { get; }
-        public decimal Rate { get; }
-
-        public ExcangeRate(string value1, string value2, decimal rate)
+        private void AddIfThereIsMirrorCoupleOfInputPairAndAlsoAddInputPairIfItIsntInLlist(List<ExchangeRate> result, List<string> isoCodesOfRates, int currency1, int currency2, ExchangeRate rate, int count)
         {
-            Currency1 = value1;
-            Currency2 = value2;
-            Rate = rate;
+            if (isoCodesOfRates[currency1].Equals(rate.Currency2) && isoCodesOfRates[currency2].Equals(rate.Currency1))
+            {
+                result.Add(new ExchangeRate(isoCodesOfRates[currency1], isoCodesOfRates[currency2], Math.Round(MIN_VALUE / rate.Rate, 7)));
+            }
+            else if (count == result.Count)
+            {
+                result.Add(new ExchangeRate(isoCodesOfRates[currency1], isoCodesOfRates[currency2], new Random().Next(MIN_VALUE, MAX_VALUE)));
+            }
         }
     }
 }
