920f49b6ea2c07064e816d2daf6415c6b60164b6
saburlaev_denis <mr.saburlaev@mail.ru>
2023-10-17 23:50:01 +0300

    The first method for parsing data from an Excel table has been written so far

---
+++ PersonalFinance.Api/PersonalFinance.Tests/CurrencyConverterTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PersonalFinance.Services;
+
+namespace PersonalFinance.Tests
+{
+    public class CurrencyConverterTest
+    {
+        private readonly CurrencyConverter _currencyConverter;
+
+        public CurrencyConverterTest()
+        {
+            _currencyConverter = new CurrencyConverter();
+        }
+
+        [Test]
+        public void chekingTest()
+        {
+            string curr1 = "RUB";
+            string curr2 = "USD";
+            decimal amount = 100;
+            decimal expected = 4600;
+
+            decimal actual = _currencyConverter.Convert(curr1, curr2, amount);
+
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+    
+}
