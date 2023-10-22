4f5cdb6dee1aa134f9b2008970149c01114ece3a
saburlaev_denis <mr.saburlaev@mail.ru>
2023-10-22 22:50:44 +0300

    some changes

--- PersonalFinance.Api/Dockerfiles/06/script.sql
+++ PersonalFinance.Api/Dockerfiles/06/script.sql
@@ -1,3 +1,7 @@
+CREATE DATABASE MyBase;
+
+USE Mybase;
+
 CREATE TABLE rates (curr1 nchar(3), curr2 nchar(3), rate decimal(18,2));
 
 INSERT INTO rates (curr1, curr2, rate) VALUES ('RUB', 'EUR', ROUND(RAND()*(50-100), 2)), ('USD', 'CAD', ROUND(RAND()*(9-1)+0.8, 2)), ('JPY', 'SAR', ROUND(RAND()*(100-10), 2));
