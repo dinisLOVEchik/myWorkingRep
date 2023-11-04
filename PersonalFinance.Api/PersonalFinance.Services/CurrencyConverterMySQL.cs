5a7b64817f0dbf4dad0b8b831680d2f4443ff319
saburlaev_denis <mr.saburlaev@mail.ru>
2023-11-04 22:53:55 +0300

    The class CurrencyConverterMySQL and class CurrencyConverterMySQLTests are created and checked. Everything are working well

---
+++ PersonalFinance.Api/PersonalFinance.Services/CurrencyConverterMySQL.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DocumentFormat.OpenXml.Drawing.Diagrams;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace PersonalFinance.Services
+{
+    public class CurrencyConverterMySQL
+    {
+        string connStr = "server=localhost;user=root;database=rates_db;password=00400040;";
+
+        public decimal Convert(string currencyFrom, string currencyTo, decimal amount)
+        {
+            decimal res = 0;
+
+            MySqlConnection conn = new MySqlConnection(connStr);
+
+            conn.Open();
+
+            string sql = "SELECT * FROM rates";
+
+            MySqlCommand command = new MySqlCommand(sql, conn);
+
+            MySqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                if (reader[0].ToString() == currencyFrom && reader[1].ToString() == currencyTo)
+                {
+                    res = Decimal.Parse(reader[2].ToString()) * amount;
+                }
+            }
+
+            return res;
+        }
+    }
+}
