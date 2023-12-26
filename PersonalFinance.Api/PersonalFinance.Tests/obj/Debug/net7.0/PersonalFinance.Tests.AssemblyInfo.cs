14150a7bfc8f21668a1371e9c98f63fbb4e5920e
saburlaev_denis <mr.saburlaev@mail.ru>
2023-12-26 21:45:03 +0300

    Have created the main Dockerfile for the PersonalFinance.Api project

--- PersonalFinance.Api/PersonalFinance.Tests/obj/Debug/net7.0/PersonalFinance.Tests.AssemblyInfo.cs
+++ PersonalFinance.Api/PersonalFinance.Tests/obj/Debug/net7.0/PersonalFinance.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PersonalFinance.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b626bb3a24e132d5f4dbda070b822a96ab95d596")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("PersonalFinance.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PersonalFinance.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
