d51119a35de205b99de00a9fe46fb5afce76dc87
Bashir Magomedov <bashir.magomedov@galytix.com>
2024-03-04 20:50:02 +0300

    creating proper Dockerfile

--- PersonalFinance.Api/PersonalFinance.Api/Program.cs
+++ PersonalFinance.Api/PersonalFinance.Api/Program.cs
@@ -1,50 +1,22 @@
-using PersonalFinance.Api;
-using PersonalFinance.Services;
-using System.Text.Json.Serialization;
-
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers().AddJsonOptions(
-    x=>
-    {
-        x.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
-    }
-    );
- //Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-var conf = new ConfigurationBuilder()
-    .SetBasePath(Directory.GetCurrentDirectory())
-    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-    .AddUserSecrets<Program>() 
-    .Build();
-
-var fxRatesProviderSettings = conf.GetSection("fxRatesProviderSettings");
-var fxRatesConnectionStrings = fxRatesProviderSettings.GetSection("FxRatesConnectionStrings");
-
-var fxRatesProviderResolver = new FxRatesProviderResolver();
-fxRatesProviderResolver.Add("CSV", new CsvRateProvider(fxRatesConnectionStrings["CsvFilePath"], ';', 30000));
-fxRatesProviderResolver.Add("MySql", new MySqlRateProvider(fxRatesConnectionStrings["MySqlConnectionString"]));
-fxRatesProviderResolver.Add("MSSQL", new SqlServerRateProvider(fxRatesConnectionStrings["SqlServerConnectionString"]));
-
-builder.Services.AddSingleton(fxRatesProviderResolver);
-
-builder.Services.AddTransient<CurrencyValidator>();
-
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+app.UseSwagger();
+app.UseSwaggerUI();
+//}
 
-//app.MapGet("api/server/ping", ()=> "pong");
-
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
