a657ade31037534fadc6a10b4de393c4b5b3f4e2
Bashir Magomedov <bashir.magomedov@galytix.com>
2024-03-04 21:04:34 +0300

    Putting back some deleted code

--- PersonalFinance.Api/PersonalFinance.Api/Program.cs
+++ PersonalFinance.Api/PersonalFinance.Api/Program.cs
@@ -1,3 +1,6 @@
+using PersonalFinance.Api;
+using PersonalFinance.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,18 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var fxRatesProviderSettings = builder.Configuration.GetSection("fxRatesProviderSettings");
+var fxRatesConnectionStrings = fxRatesProviderSettings.GetSection("FxRatesConnectionStrings");
+
+var fxRatesProviderResolver = new FxRatesProviderResolver();
+fxRatesProviderResolver.Add("CSV", new CsvRateProvider(fxRatesConnectionStrings["CsvFilePath"], ';', 30000));
+fxRatesProviderResolver.Add("MySql", new MySqlRateProvider(fxRatesConnectionStrings["MySqlConnectionString"]));
+fxRatesProviderResolver.Add("MSSQL", new SqlServerRateProvider(fxRatesConnectionStrings["SqlServerConnectionString"]));
+
+builder.Services.AddSingleton(fxRatesProviderResolver);
+
+builder.Services.AddTransient<CurrencyValidator>();
 
 var app = builder.Build();
 
